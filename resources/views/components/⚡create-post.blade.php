ec2783218d7f93512a6dcb20c340e7d05f7e6cdf
cylee1668 <cylee1668@gmail.com>
2026-04-24 00:48:59 +0800

    counter in alipine

--- resources/views/components/⚡create-post.blade.php
+++ resources/views/components/⚡create-post.blade.php
@@ -2,6 +2,7 @@
 
 use Livewire\Component;
 use App\Models\Post;
+use Livewire\Attributes\Rule;
 
 new class extends Component {
     // public function render()
@@ -9,17 +10,16 @@ new class extends Component {
     //     return view('create-post');
     // }
 
+    #[Rule('required', message: 'Yo, the title')]
+    #[Rule('min:4', message: 'Yo, short')]
     public $title = '';
 
+    #[Rule('required', as: '中文da context')]
     public $content = '';
 
     public function save()
     {
-
-        $this->validate([
-            'title' => 'required',
-            'content' => 'required',
-        ]);
+        $this->validate();
 
         Post::create([
             'title' => $this->title,
@@ -33,11 +33,19 @@ new class extends Component {
 
 <div>
     <h2>New Post:</h2>
+
+    <div x-data="{ count: 0 }">
+        <span x-text="count"></span>
+        <button x-on:click="count++">+</button>
+    </div>
+
     <form wire:submit="save">
         <label>
             <span>Title</span>
             <input type="text" wire:model="title">
-            @error('title') <em>{{ $message }}</em>@enderror
+            @error('title')
+                <em>{{ $message }}</em>
+            @enderror
 
 
         </label>
@@ -45,7 +53,9 @@ new class extends Component {
         <label>
             <span>Content</span>
             <textarea wire:model="content"></textarea>
-            @error('content') <em>{{ $message }}</em>@enderror
+            @error('content')
+                <em>{{ $message }}</em>
+            @enderror
             <button type="submit">Save</button>
         </label>
     </form>
